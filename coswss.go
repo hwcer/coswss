1b9642ffea45423418544cfa975d487fc7f52c18
万超 黄 <hwclegend@163.com>
2024-12-26 21:08:39 +0800

    update

--- coswss.go
+++ coswss.go
@@ -4,43 +4,22 @@ import (
 	"crypto/tls"
 	"errors"
 	"github.com/gorilla/websocket"
-	"github.com/hwcer/cosgo/logger"
 	"github.com/hwcer/cosgo/scc"
-	"github.com/hwcer/cosnet"
 	"github.com/hwcer/cosweb"
 	"net/http"
 	"sync/atomic"
 	"time"
 )
 
-func New() *Server {
-	ln := &Server{}
-	ln.upgrader = websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-	}
-	ln.upgrader.CheckOrigin = ln.AccessControlAllow
-	return ln
+var started int32
+var upgrader = websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024}
+var httpServer []*http.Server
+
+func init() {
+	upgrader.CheckOrigin = AccessControlAllow
 }
 
-type Server struct {
-	//Accept   func(s *cosnet.Socket, uid string)
-	//Verify   func(w http.ResponseWriter, r *http.Request) (uid string, err error)
-	//Origin   []string
-	httpSrv  *http.Server
-	started  int32
-	upgrader websocket.Upgrader
-}
-
-func (s *Server) HTTPErrorHandler(w http.ResponseWriter, r *http.Request, err error) {
-	w.WriteHeader(500)
-	if r.Method != http.MethodHead {
-		_, _ = w.Write([]byte(err.Error()))
-	}
-	logger.Alert(err)
-}
-
-func (s *Server) AccessControlAllow(r *http.Request) bool {
+func AccessControlAllow(r *http.Request) bool {
 	if len(Options.Origin) == 0 {
 		return true
 	}
@@ -52,66 +31,32 @@ func (s *Server) AccessControlAllow(r *http.Request) bool {
 	return false
 }
 
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if scc.Stopped() {
-		s.HTTPErrorHandler(w, r, errors.New("server is stopped"))
-		return
-	}
-	var err error
-	var meta map[string]string
-	if Options.Verify != nil {
-		meta, err = Options.Verify(w, r)
-	}
-	if err != nil {
-		s.HTTPErrorHandler(w, r, err)
-		return
+func start() {
+	if atomic.CompareAndSwapInt32(&started, 0, 1) {
+		scc.Trigger(stopped)
 	}
 
-	var header = map[string][]string{"Sec-WebSocket-Protocol": {r.Header.Get("Sec-WebSocket-Protocol")}}
-
-	conn, err := s.upgrader.Upgrade(w, r, header)
-	if err != nil {
-		s.HTTPErrorHandler(w, r, err)
-		return
-	}
-	var sock *cosnet.Socket
-	sock, err = cosnet.New(NewConn(conn))
-	if err != nil {
-		s.HTTPErrorHandler(w, r, err)
-		return
-	}
-	if Options.Accept != nil {
-		Options.Accept(sock, meta)
+}
+func stopped() {
+	for _, h := range httpServer {
+		_ = h.Close()
 	}
 }
-func (s *Server) handle(c *cosweb.Context, next cosweb.Next) error {
-	s.ServeHTTP(c.Response, c.Request)
+
+func Binding(srv *cosweb.Server, route string) error {
+	h := &handler{}
+	srv.Register(route, h.handle)
 	return nil
 }
-func (s *Server) Binding(srv *cosweb.Server, route string) {
-	srv.Register(route, s.handle)
-	s.start()
-}
 
-func (s *Server) start() {
-	if atomic.CompareAndSwapInt32(&s.started, 0, 1) {
-		scc.Trigger(s.stopped)
-	}
-
-}
-func (s *Server) stopped() {
-	if s.httpSrv != nil {
-		_ = s.httpSrv.Close()
-	}
-}
-
-func (s *Server) Start(address string, tlsConfig ...*tls.Config) (err error) {
+func Listen(address string, route string, tlsConfig ...*tls.Config) (err error) {
+	h := &handler{route: route}
 	srv := &http.Server{
 		Addr:              address,
 		ReadHeaderTimeout: 3 * time.Second,
-		Handler:           s,
+		Handler:           h,
 	}
-	s.httpSrv = srv
+	httpServer = append(httpServer, srv)
 	if len(tlsConfig) > 0 {
 		srv.TLSConfig = tlsConfig[0]
 	}
@@ -127,7 +72,7 @@ func (s *Server) Start(address string, tlsConfig ...*tls.Config) (err error) {
 		err = nil
 	}
 	if err == nil {
-		s.start()
+		start()
 	}
 	return
 }
---
+++ handler.go
@@ -0,0 +1,66 @@
+package coswss
+
+import (
+	"errors"
+	"github.com/hwcer/cosgo/logger"
+	"github.com/hwcer/cosgo/scc"
+	"github.com/hwcer/cosnet"
+	"github.com/hwcer/cosweb"
+	"net/http"
+)
+
+type handler struct {
+	route string
+}
+
+func (s *handler) handle(c *cosweb.Context, next cosweb.Next) error {
+	s.ServeHTTP(c.Response, c.Request)
+	return nil
+}
+
+func (s *handler) HTTPErrorHandler(w http.ResponseWriter, r *http.Request, err error) {
+	w.WriteHeader(500)
+	if r.Method != http.MethodHead {
+		_, _ = w.Write([]byte(err.Error()))
+	}
+	logger.Alert(err)
+}
+
+func (s *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if scc.Stopped() {
+		s.HTTPErrorHandler(w, r, errors.New("server is stopped"))
+		return
+	}
+	if s.route != "" && r.URL.Path != s.route {
+		w.WriteHeader(http.StatusNotFound)
+		_, _ = w.Write([]byte("404 page not found"))
+		return
+	}
+
+	var err error
+	var meta map[string]string
+	if Options.Verify != nil {
+		meta, err = Options.Verify(w, r)
+	}
+	if err != nil {
+		s.HTTPErrorHandler(w, r, err)
+		return
+	}
+
+	var header = map[string][]string{"Sec-WebSocket-Protocol": {r.Header.Get("Sec-WebSocket-Protocol")}}
+
+	conn, err := upgrader.Upgrade(w, r, header)
+	if err != nil {
+		s.HTTPErrorHandler(w, r, err)
+		return
+	}
+	var sock *cosnet.Socket
+	sock, err = cosnet.New(NewConn(conn))
+	if err != nil {
+		s.HTTPErrorHandler(w, r, err)
+		return
+	}
+	if Options.Accept != nil {
+		Options.Accept(sock, meta)
+	}
+}
